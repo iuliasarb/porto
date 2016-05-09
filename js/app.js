8ddfd8b7f7f47b458b67364890b335d693ee0b21
iuliasarb <sarb_iulia@yahoo.com>
2016-05-09 16:45:33 +0200

    added text rotator with random text

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
-// ===== smooth scroll ===== 
+
 $(function() {
+  
+ // ===== text rotator ===== 
+ var arr = [
+    'I am',
+    'I\'m awesome!',
+    'I\'m an explorer',
+    'I\'m just cool',
+    'Don\'t you think?'
+    ];
+  
+  $('.rotator')
+    .data('intervalLink', window.setInterval(function() {
+      var i = arguments.callee.i ? arguments.callee.i : 0,
+        message = arr[i];
+      $('.rotator').animate({
+        opacity: 0
+      }, 500, function() {
+        $(this).html(message);
+      }).animate({
+        opacity: 1
+      }, 500);
+      arguments.callee.i = (i++ == (arr.length - 1)) ? 0 : i++;
+    }, 3000));
+  
+  // ===== smooth scroll ===== 
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -12,17 +37,60 @@ $(function() {
       }
     }
   });
+
 });
 
+/* asta merge!!!!
 
+(function($){
+    $.fn.extend({ 
+        rotaterator: function(options) {
+ 
+            var defaults = {
+                fadeSpeed: 500,
+                pauseSpeed: 100,
+				child:null
+            };
+             
+            var options = $.extend(defaults, options);
+         
+            return this.each(function() {
+                  var o =options;
+                  var obj = $(this);                
+                  var items = $(obj.children(), obj);
+				  items.each(function() {$(this).hide();})
+				  if(!o.child){var next = $(obj).children(':first');
+				  }else{var next = o.child;
+				  }
+				  $(next).fadeIn(o.fadeSpeed, function() {
+						$(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
+							var next = $(this).next();
+							if (next.length == 0){
+									next = $(obj).children(':first');
+							}
+							$(obj).rotaterator({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
+						})
+					});
+            });
+        }
+    });
+})(jQuery);
 
+ $(document).ready(function() {
+        $('#rotate').rotaterator({fadeSpeed:500, pauseSpeed:100});
+ });
+
+pana aici!!!!!*/ 
+
+/*
 function nextMsg() {
     if (messages.length == 0) {
         // once there is no more message, do whatever you want
         messages.length == true; //aici era alertul
     } else {
         // change content of message, fade in, wait, fade out and continue with next message
-        $('#message').html(messages.shift()).fadeIn(500).delay(1000).fadeOut(500, nextMsg);
+       var i = $('#message').html(messages.shift()).fadeIn(500).delay(1000).fadeOut(500, nextMsg);
+	   messages += i;
     }
 };
 // list of messages to display
